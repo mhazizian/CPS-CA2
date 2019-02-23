1ab29f237f6683b9398c727df83192fa3dbf3540
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-02-23 10:10:34 +0330

    Use 20*4 lcd

--- Main Board/src/main.cpp
+++ Main Board/src/main.cpp
@@ -15,7 +15,7 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  lcd.begin(16, 2);
+  lcd.begin(20, 4);
   Serial.begin(9600);
 }
 
